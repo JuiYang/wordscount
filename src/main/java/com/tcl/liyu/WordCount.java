5bf7d09a175ea1da46b5557a46582ef37d3463d5
JuiYang <894139169@qq.com>
2018-11-13 10:01:55 +0800

    整理代码

--- src/main/java/com/tcl/liyu/WordCount.java
+++ src/main/java/com/tcl/liyu/WordCount.java
@@ -19,34 +19,30 @@ public class WordCount {
         try{
             // 获取文件系统
             FileSystem fileSystem = FileSystem.get(conf);
-            // 创建任务
+            // 创建Job对象
             Job job = Job.getInstance(conf);
-//            设置Job的入口类
+            // 设置Job的主程序入口
             job.setJarByClass(WordCount.class);
-            // Job执行的时候给它一个名称，也可以不给（会有默认值）
-             job.setJobName("wordcount");
-            // Job执行的时候map的任务类是哪个类
-             job.setMapperClass(MyMapper.class);
-             job.setReducerClass(MyReducer.class);
-            // maptask输出键的类型（以单词为键所以用Text.class）
-             job.setMapOutputKeyClass(Text.class);
-            // maptask输出value的类型（以数值为值所以用IntWritable.class）
+            // 设置任务名称
+            job.setJobName("wordcount");
+            // 设置Map输入key-value数据类型
+            job.setMapperClass(MyMapper.class);
+            job.setReducerClass(MyReducer.class);
+            // 设置Map输出key-value数据类型
+            job.setMapOutputKeyClass(Text.class);
             job.setMapOutputValueClass(IntWritable.class);
 
 
-            /*定义输入的数据的目录（在hdfs中的某一个文件）,是运行程序的输出结果，提前建好usr和output就行，不用建wc,到时候会自动生成*/
+            // 设置文件输入路径
             FileInputFormat.addInputPath(job, new Path("/data/wordcount/"));
-            /*定义输出的数据的目录（在hdfs中的某一个文件），目录是在hdfs中txt文件所在的目录，在input文件夹下可以同时有好几个txt，下面路径只要写到input就行，会自动检测到底下的所有txt文件。*/
+            // 设置文件输出路径
             Path outpath =new Path("/data/output");
             if(fileSystem.exists(outpath)){
                 fileSystem.delete(outpath, true);
             }
             FileOutputFormat.setOutputPath(job, outpath);
             //等待任务执行（返回是否成功）
-            boolean result= job.waitForCompletion(true);
-            if(result){
-                System.out.println("job任务执行成功");
-            }
+            System.exit(job.waitForCompletion(true)? 0 : -1);
 
         }catch (Exception e){
             e.printStackTrace();
